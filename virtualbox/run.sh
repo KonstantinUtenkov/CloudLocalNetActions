7a6905a6711f8b6e63bd3a96b0f8b3e8792d3690
Konstantin Utenkov <dev@cloudlocalnet.com>
2023-03-04 17:13:15 +0000

    add ext pack install

--- virtualbox/run.sh
+++ virtualbox/run.sh
@@ -4,4 +4,5 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt update
 apt -y install virtualbox
+echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections
 apt -yq install virtualbox-ext-pack
