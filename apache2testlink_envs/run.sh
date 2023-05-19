53a6c8837074773621c683e83943a194343dee81
Konstantin Utenkov <dev@cloudlocalnet.com>
2023-05-19 08:07:37 +0000

    add centered page

--- apache2testlink_envs/run.sh
+++ apache2testlink_envs/run.sh
@@ -9,13 +9,29 @@
 echo "<html xmlns=\"http://www.w3.org/1999/xhtml\">
   <head>
     <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
+    <meta name=\"viewport\" content=\"width=device-width\">
     <title>$TITLE</title>
   </head>
 <body>
 
-$CONTENT
 
+<center>
+<table border=0 width=100% height=100%>
+<tr align=center>
+<td>
+
+<br>
+$CONTENT
+<br>
 <b> $CONTENT_F </b>
+<br>
+
+</td>
+</tr>
+</table>
+
+
+</center>
 
 <body>
 </html>
