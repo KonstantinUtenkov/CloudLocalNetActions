ef49920269ae31c93c114e5e0a3718bd2d422f0f
Konstantin Utenkov <dev@cloudlocalnet.com>
2023-05-20 13:23:37 +0000

    add nginx action

---
+++ nginxtestlink_env/run.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+
+echo "start install nginx"
+apt -y update
+apt -y install bash nginx
+#env
+
+echo "<html xmlns=\"http://www.w3.org/1999/xhtml\">
+  <head>
+    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
+    <meta name=\"viewport\" content=\"width=device-width\">
+    <title>$TITLE</title>
+  </head>
+<body>
+
+
+<center>
+<table border=0 width=100% height=100%>
+<tr align=center>
+<td>
+
+<br>
+$CONTENT
+<br>
+<b> $CONTENT_F </b>
+<br>
+
+</td>
+</tr>
+</table>
+
+
+</center>
+
+<body>
+</html>
+" > /var/www/html/index.html
+
+nginx -c /etc/nginx/nginx.conf
+
+echo "end install nginx"
