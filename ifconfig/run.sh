0518d3b1d805e3f80555dd0761787c9716c477a4
KonstantinUtenkov <65397320+KonstantinUtenkov@users.noreply.github.com>
2024-03-17 16:44:47 +0300

    Update run.sh
    grep local nets

--- ifconfig/run.sh
+++ ifconfig/run.sh
@@ -5,6 +5,9 @@ echo "start install bash"
 apt -y update
 apt -y install bash net-tools
 
-ifconfig -a
+#ifconfig -a
+
+echo "IP address exclude 172.x.x.x and 127.x.x.x:"
+ifconfig -a | grep "inet " | grep -v " 127." | grep -v " 172.".
 
 echo "end install bash"
