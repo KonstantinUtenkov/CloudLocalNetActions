20e8846323122cf2ce3dcd7e90dca92768f6d4e0
root <root@dev.cloudlocalnet.com>
2024-09-28 14:32:26 +0200

    add custom hostname action

---
+++ renamehost/README.md
@@ -0,0 +1,3 @@
+Rename hostname from ENV
+
+
---
+++ renamehost/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+echo "start rename hostname"
+apt -y update
+apt -y install bash
+
+systemctl disable randomhostname
+
+cat $NEWHOSTNAME > /etc/hostname
+
+
+
+#env
+
+
+echo "end rename hostname"
